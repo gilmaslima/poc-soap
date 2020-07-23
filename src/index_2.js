9b29e7c38925f95fa86ef0d41d574c682e5f15ec
Gilmar Lima <gilmaslima@gmail.com>
2020-07-23 18:21:45 -0300

    Initial commit

--- src/index_2.js
+++
@@ -1,8 +0,0 @@
-var soap = require('soap');
-var url = 'http://www.gcomputer.net/webservices/dilbert.asmx?wsdl';
-soap.createClient(url, function(err, client) {
-    client.DailyDilbert({ ADate: "2019-11-02" }, function(err, result) {
-        if(err) return console.log(err);
-        console.log(result.DailyDilbertResult);
-    });
-});
